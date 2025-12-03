3570086f927164a394598b4444c1cdf43f7dddd7
unknown <vishwachary.varanasi@gmail.com>
2025-12-03 15:44:14 +0530

    rename

--- src/main/java/com/example/JavaUserGroupEventsApplication.java
+++ src/main/java/com/example/JavaUserGroupEventsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VoucherManagementSystemApplication {
+public class JavaUserGroupEventsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VoucherManagementSystemApplication.class, args);
+		SpringApplication.run(JavaUserGroupEventsApplication.class, args);
 	}
 
 }
